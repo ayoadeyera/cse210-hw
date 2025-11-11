08605f96fe29289d1e6bcc365323f3579df9b175
ayoadeyera <guitarsolomon@gmail.com>
2025-11-11 10:33:53 +0100

    Grade calculator created

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("Enter your Score number to get your grade: ");
+        string userInput = Console.ReadLine();
+        int userScore = int.Parse(userInput);
+
+        string letter = "";
+
+        if (userScore >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (userScore >= 80)    
+        {
+            letter = "B";
+        }
+
+        else if (userScore >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (userScore >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (userScore >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass. Better luck next time!");
+        }
     }
 }
